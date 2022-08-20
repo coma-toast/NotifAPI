9d1292bd3da4fd6a6358ecbd9ad1aa77806274fd
Jason Dale <jdale215@gmail.com>
2022-08-19 23:03:16 -0400

    /api/notify works

--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
+	"os"
+
 	"github.com/coma-toast/notifapi/internal/utils"
-	"github.com/coma-toast/notifapi/pkg/notification"
+	"github.com/coma-toast/notifapi/pkg/api"
+	"github.com/coma-toast/notifapi/pkg/app"
 	"github.com/coma-toast/notifapi/pkg/pusher"
 )
 
-type App struct {
-	Config   utils.Config
-	Data     utils.DataModel
-	Logger   utils.Logger
-	Notifier notification.Notifier
-}
-
 func main() {
-	app := App{}
+	app := app.App{}
 
 	app.Config = *utils.GetConf()
 	app.Logger.Init(false)
-	app.Notifier = pusher.Pusher{InstanceID: app.Config.InstanceID, SecretKey: app.Config.SecretKey}
 	app.Logger.Info("App initialized")
 	app.Data.Init(app.Config.DBFilePath)
-	data := map[string]interface{}{
-		"test data": "asdf",
+	hostname, err := os.Hostname()
+	if err != nil {
+		app.Logger.Error(err)
 	}
-	app.Notifier.SendMessage([]string{"hello"}, "Test notification", "This is a test", "", data)
-	// go run API
-	// go run HTTP
+	app.Notifier = pusher.Pusher{InstanceID: app.Config.InstanceID, SecretKey: app.Config.SecretKey, Data: &app.Data}
+	id, err := app.Notifier.SendMessage([]string{"hello"}, "NotifAPI", "NotifAPI is starting up on "+hostname, "main.go")
+	if err != nil {
+		app.Logger.ErrorWithField("Error sending message", "interest", "hello")
+	}
+	app.Logger.Debug(id)
+
+	api := api.API{App: &app}
+
+	api.RunAPI()
 
 }
--- pkg/notification/notification.go
+++ pkg/notification/notification.go
@@ -1,5 +1,16 @@
 package notification
 
+type Message struct {
+	Interests []string               `json:"interests"`
+	Title     string                 `json:"title"`
+	Body      string                 `json:"body"`
+	Link      string                 `json:"link"`
+	Source    string                 `json:"source"`
+	Metadata  map[string]interface{} `json:"metadata"`
+}
+
 type Notifier interface {
-	SendMessage(interests []string, title, message, link string, metadata map[string]interface{}) error
+	SendMessage(interests []string, title, body, source string) (string, error)
+	SendMessageWithLink(interests []string, title, body, link, source string) (string, error)
+	SendMessageFull(interests []string, title, body, link, source string, metadata map[string]interface{}) (string, error)
 }
