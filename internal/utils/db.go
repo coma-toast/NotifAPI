e56149c0be4f79e9c85bbe76070bc761d7ffd82b
Jason Dale <jdale215@gmail.com>
2022-08-19 23:03:16 -0400

    /api/notify works

--- internal/utils/db.go
+++ internal/utils/db.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -12,6 +15,16 @@ type DataModel struct {
 	DB *sqlx.DB
 }
 
+type NotificationRow struct {
+	PubID     string `db:"pub_id" json:"pub_id"`
+	Date      string `db:"date" json:"date"`
+	Source    string `db:"source" json:"source"`
+	Interests string `db:"interests" json:"interests"`
+	Title     string `db:"title" json:"title"`
+	Message   string `db:"message" json:"message"`
+	Metadata  string `db:"metadata" json:"metadata"`
+}
+
 func (d *DataModel) Init(location string) {
 	err := os.MkdirAll(location, os.ModePerm)
 	if err != nil {
@@ -19,4 +32,70 @@ func (d *DataModel) Init(location string) {
 	}
 
 	d.DB = sqlx.MustConnect("sqlite3", location+"/data.db")
+
+	schema := `CREATE TABLE IF NOT EXISTS notifications (
+		pub_id text PRIMARY KEY,
+		date TEXT DEFAULT CURRENT_TIMESTAMP,
+		source TEXT,
+		interests TEXT,
+		title TEXT,
+		message TEXT,
+		metadata TEXT
+	);`
+
+	result := d.DB.MustExec(schema)
+	fmt.Println("DB Initialized", result)
+}
+
+func (d *DataModel) AddNotification(pubID, source, title, message string, interests []string, metadata map[string]interface{}) (sql.Result, error) {
+	insert := `INSERT INTO notifications 
+	(
+		pub_id,
+		source,
+		interests,
+		title,
+		message,
+		metadata
+	)
+	VALUES 
+	(
+		?,
+		?,
+		?,
+		?,
+		?,
+		?
+	);`
+
+	jsonInterests, err := json.Marshal(interests)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonMetadata, err := json.Marshal(metadata)
+	if err != nil {
+		return nil, err
+	}
+
+	return d.DB.Exec(insert, pubID, source, jsonInterests, title, message, jsonMetadata)
+}
+
+func (d *DataModel) GetRecentNotifications(limit int) ([]NotificationRow, error) {
+	notifications := make([]NotificationRow, 0)
+	statement := `SELECT * FROM notifications ORDER BY date LIMIT ?`
+	rows, err := d.DB.Queryx(statement, limit)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var row NotificationRow
+		err := rows.StructScan(&row)
+		if err != nil {
+			return nil, err
+		}
+		notifications = append(notifications, row)
+	}
+
+	return notifications, nil
+
 }
