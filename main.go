23c504b82e97581758be53b29b09082d255ba9e2
Jason Dale <jdale215@gmail.com>
2022-08-23 21:38:07 -0400

    remove dev code

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
-	"time"
 
 	"github.com/coma-toast/notifapi/internal/utils"
 	"github.com/coma-toast/notifapi/pkg/api"
 	"github.com/coma-toast/notifapi/pkg/app"
-	"github.com/coma-toast/notifapi/pkg/client"
-	"github.com/coma-toast/notifapi/pkg/notification"
 	"github.com/coma-toast/notifapi/pkg/pusher"
 )
 
@@ -39,22 +35,6 @@ func main() {
 
 	go api.RunAPI()
 
-	//* dev code
-	time.Sleep(time.Second * 5)
-	client := client.Client{Target: "http://127.0.0.1:10887"}
-	message := notification.Message{
-		Interests: []string{"hello"},
-		Title:     "Test from client",
-		Body:      "This is a test from the client",
-		Source:    "main.go",
-	}
-
-	response, err := client.SendMessage(message)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(response.Status)
-	fmt.Println("message sent")
 	dontExit := make(chan bool)
 	// Waiting for a channel that never comes...
 	<-dontExit
